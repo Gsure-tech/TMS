797d87418dc12ff1705bfa10ca1794365fdd2dd8
gsure-tech <Geeescode@>
2024-12-07 13:55:35 +0100

    reading the items from the file

--- main.py
+++ main.py
@@ -7,16 +7,21 @@ while True:
 
             file = open('todos.txt', 'r')
             todos = file.readlines()
+            file.close()
 
             todos.append(todo)
 
             file = open('todos.txt','w')
             file.writelines(todos)
+            file.close()
         case 'show' :
+            file = open('todos.txt', 'r')
+            todos = file.readlines()
+            file.close()
+
             for index, item in enumerate(todos):
                 row = f"{index + 1}-{item}"
                 print(row)
-            print("Hello", index, item )
         case 'edit':
             number = int(input("Number of the todo to edit"))
             number = number -1
