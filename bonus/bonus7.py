9a756016ee67f40399b2613ed11fb3c8d10ac7f9
gsure-tech <Geeescode@>
2024-12-08 17:32:06 +0100

    Added bonus 7

--- bonus/bonus7.py
+++ bonus/bonus7.py
@@ -7,3 +7,8 @@ print(filenames)
 usernames = ["john 1990", "alberta1970", "magnola2000"]
 usernames = [len(username) for username in usernames]
 print(usernames)
+
+
+user_entries = ['10', '19.1', '20']
+user_entries = [float(user_entry) for user_entry in user_entries]
+print(user_entries)
