70ec074d895e63be07167deebbe7bb9f030ba14e
gsure-tech <Geeescode@>
2024-12-08 17:19:10 +0100

    Added bonus 7

--- bonus/bonus7.py
+++ bonus/bonus7.py
@@ -2,3 +2,8 @@ filenames = ["1.doc","1.report","1.presentation"]
 
 filenames = [filename.replace('.','_')+".txt" for filename in filenames]
 print(filenames)
+
+
+usernames = ["john 1990", "alberta1970", "magnola2000"]
+usernames = [len(username) for username in usernames]
+print(usernames)
