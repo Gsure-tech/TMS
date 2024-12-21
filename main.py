32ad3ca81e853d5317d473126f2cf9fe6d65531a
gsure-tech <Geeescode@>
2024-12-21 15:11:14 +0100

    Modified the list comprehension using with

--- main.py
+++ main.py
@@ -55,7 +55,14 @@ while True:
 
         case 'complete':
             number = int(input("Number of the todo to complete"))
+
+            with open('files/todos.txt', 'r') as file:
+                todos = file.readlines()
+
             todos.pop(number - 1)
+
+            with open('files/todos.txt', 'w') as file:
+                file.writelines(todos)
         case 'exit':
             break
 
