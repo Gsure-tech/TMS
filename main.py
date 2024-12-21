0821228c8f0930986a9962cd311c7f0a1919aaff
gsure-tech <Geeescode@>
2024-12-21 12:39:50 +0100

    Modified the list comprehension using with

--- main.py
+++ main.py
@@ -5,20 +5,29 @@ while True:
         case 'add':
             todo = input("Enter a todo") + "\n"
 
-            file = open('files/todos.txt', 'r')
-            todos = file.readlines()
-            file.close()
+            # file = open('files/todos.txt', 'r')
+            # todos = file.readlines()
+            # file.close()
+
+            with open('files/todos.txt', 'r') as file:
+                todos = file.readlines()
 
             todos.append(todo)
 
-            file = open('files/todos.txt', 'w')
-            file.writelines(todos)
-            file.close()
+            # file = open('files/todos.txt', 'w')
+            # file.writelines(todos)
+            # file.close()
+
+            with open('files/todos.txt','w') as  file:
+                file.writelines(todos)
         case 'show' :
 
-            file = open('files/todos.txt', 'r')
-            todos = file.readlines()
-            file.close()
+            # file = open('files/todos.txt', 'r')
+            # todos = file.readlines()
+            # file.close()
+
+            with open('files/todos.txt','r') as file:
+                todos = file.readlines()
 
             # new_todos = []
             # for item in todos:
