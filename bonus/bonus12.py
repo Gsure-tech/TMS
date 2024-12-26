96c59155ba5ddc705e323a84aab1de58e6e61322
gsure-tech <Geeescode@>
2024-12-26 18:11:39 +0100

    code decoupling

--- bonus/bonus12.py
+++ bonus/bonus12.py
@@ -1,18 +1,20 @@
-from os.path import split
-
 feet_inches = input("Enter feet and inches: ")
 
-def convert(feet_inches):
-    parts = feet_inches.split(" ")
+def parse(feetinches):
+    parts = feetinches.split(" ")
     feet = float(parts[0])
     inches = float(parts[1])
+    return feet, inches
 
+
+def convert(feet, inches):
     meters = feet * 0.3048 + inches * 0.254
-
     # return f"{feet} feet and {inches} inches is equal to {meters} meters."
     return  meters
 
-result = convert(feet_inches)
+f,i = parse(feet_inches)
+print("fi", f,i)
+result = convert(f,i)
 
 if result < 1:
     print("Kid is too small.")
