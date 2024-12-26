5feec5421055165947e74ecc0f119afaf84a34be
gsure-tech <Geeescode@>
2024-12-26 15:55:13 +0100

    added bug fix

--- experiments/bugfixes.py
+++ experiments/bugfixes.py
@@ -1,12 +1,18 @@
-def get_maximum():
-    celsius_local = [14, 15.1, 12.3]
-    maximum = max(celsius_local)
-    return maximum
+# def get_maximum():
+#     celsius_local = [14, 15.1, 12.3]
+#     maximum = max(celsius_local)
+#     return maximum
+#
+#
+# celsius = get_maximum()
+# fahrenheit = celsius * 1.8 + 32
+#
+# print(fahrenheit)
+
+def speed(distance, time):
+    return distance / time
 
 
-celsius = get_maximum()
-fahrenheit = celsius * 1.8 + 32
-
-print(fahrenheit)
+print(speed(200, 4))
 
 
