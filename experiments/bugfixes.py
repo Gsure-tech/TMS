819f26f88ea3914733b3b66581f3e2d1e377348e
gsure-tech <Geeescode@>
2024-12-27 10:55:20 +0100

    added bug fix

--- experiments/bugfixes.py
+++ experiments/bugfixes.py
@@ -8,11 +8,22 @@
 # fahrenheit = celsius * 1.8 + 32
 #
 # print(fahrenheit)
-
+"""
 def speed(distance, time):
     return distance / time
 
 
 print(speed(200, 4))
+
+"""
+
+
+def calculate_time( h,g=9.80665):
+    t = (2 * h / g) ** 0.5
+    return t
+
+
+time = calculate_time(100)
+print(time)
 
 
