06574e7dc6b832579fcc1aee9e5438bb7631e4cd
gsure-tech <Geeescode@>
2024-12-27 15:52:54 +0100

    added modules to seperate concerns

--- main.py
+++ main.py
@@ -1,18 +1,20 @@
-from functions import get_todos, write_todos
+# from functions import get_todos, write_todos
+from modules import functions
+
 while True:
     user_action = input("Type add, show, edit, complete or exit: ")
     user_action = user_action.strip()
 
     if user_action.startswith('add'):
         todo = user_action[4:]
-        todos = get_todos()
+        todos = functions.get_todos()
         todos.append(todo + '\n')
 
-        write_todos(todos)
+        functions.write_todos(todos)
 
     elif user_action.startswith('show'):
 
-        todos = get_todos()
+        todos = functions.get_todos()
 
         for index, item in enumerate(todos):
             item = item.strip('\n')
@@ -23,12 +25,12 @@ while True:
             number = int(user_action[5:])
             number = number -1
 
-            todos = get_todos()
+            todos = functions.get_todos()
 
             new_todo = input("Enter new todo")
             todos[number] = new_todo + '\n'
 
-            write_todos(todos)
+            functions.write_todos(todos)
 
         except ValueError:
             print("Your command is not valid.")
@@ -38,13 +40,13 @@ while True:
         try:
             number = int(user_action[9:])
 
-            todos = get_todos()
+            todos = functions.get_todos()
 
             index = number -1
             todo_to_remove = todos[index].strip('\n')
             todos.pop(index)
 
-            write_todos(todos)
+            functions.write_todos(todos)
 
             message = f"Todo {todo_to_remove} was removed from the list."
             print(message)
