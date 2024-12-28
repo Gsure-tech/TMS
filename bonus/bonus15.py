3ef6d044d1807b60c0ded362aa2525c4cab342af
gsure-tech <Geeescode@>
2024-12-28 16:10:09 +0100

    printing question

--- bonus/bonus15.py
+++ bonus/bonus15.py
@@ -3,5 +3,11 @@ import  json
 with open("questions.json", "r") as file:
     content = file.read()
 
+data = json.loads(content)
+#
+# print(type(content))
+# print(type(data))
+# print(data)
 
-print(content)
+for question in data:
+    print(question["question_text"])
