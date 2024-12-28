da2fb0afb7b8e2d6824b0cc7150849ad5dc6bd60
gsure-tech <Geeescode@>
2024-12-28 13:02:39 +0100

    using the csv  module

--- experiments/csv_usage.py
+++ experiments/csv_usage.py
@@ -4,4 +4,10 @@ with open("../files/weather.csv", "r") as file:
     data = list(csv.reader(file))
 
 
-print(data)
+# print(data)
+
+city = input("Enter a city")
+
+for row in data[1:]:
+    if row[0] == city:
+        print(row[1])
