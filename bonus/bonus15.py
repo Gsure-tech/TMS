f7d78c4e9bd01cec5927783de594b9b4a694155e
gsure-tech <Geeescode@>
2024-12-29 11:59:17 +0100

    added index and enumerate

--- bonus/bonus15.py
+++ bonus/bonus15.py
@@ -11,3 +11,5 @@ data = json.loads(content)
 
 for question in data:
     print(question["question_text"])
+    for index, alternative in enumerate(question["alternatives"]):
+        print(index + 1, "-", alternative)
