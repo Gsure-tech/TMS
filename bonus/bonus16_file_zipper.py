16cc1f4ff53c9a0b486f830c73714f113214a11d
gsure-tech <Geeescode@>
2024-12-30 09:46:59 +0100

    adding event

--- bonus/bonus16_file_zipper.py
+++ bonus/bonus16_file_zipper.py
@@ -8,6 +8,10 @@ label_destination = sg.Text("Select destination folder:")
 input_box2 = sg.InputText()
 choose_button2 = sg.FolderBrowse("Choose")
 
-window = sg.Window('File Zipper', layout=[[label_source, input_box1, choose_button1], [label_destination, input_box2, choose_button2]])
+compress_button = sg.Button("Compress")
+
+window = sg.Window('File Zipper', layout=[[label_source, input_box1, choose_button1],
+                                          [label_destination, input_box2, choose_button2],
+                                          [compress_button]])
 window.read()
 window.close()
--- gui.py
+++ gui.py
@@ -3,9 +3,15 @@ import FreeSimpleGUI as sg
 
 
 label = sg.Text("Type in a to-do")
-input_box = sg.InputText(tooltip="Enter todo")
+input_box = sg.InputText(tooltip="Enter todo", key="todo")
 add_button = sg.Button("Add")
 
-window = sg.Window('My To-Do App', layout=[[label], [input_box, add_button]])
-window.read()
+window = sg.Window('My To-Do App',
+                   layout=[[label], [input_box, add_button]],
+                   font=('Helvetica',20))
+
+
+event = window.read()
+print(event)
+
 window.close()
