38748faad1f61490bd1372026ceffdb71f099bb0
gsure-tech <Geeescode@>
2025-01-01 17:59:44 +0100

    inches converter GUI

--- bonus/inches_converter.py
+++ bonus/inches_converter.py
@@ -1,8 +1,24 @@
 import FreeSimpleGUI as sg
+from converters14 import convert
 
 label_feet = sg.Text("Enter feet")
 input_box_feet = sg.InputText(key="feet")
 label_inches = sg.Text("Enter inches")
 input_box_inches = sg.InputText(key="inches")
 convert_button = sg.Button("Convert")
-label_result = sg.Text(key="result")
+label_result = sg.Text("",key="output")
+
+window = sg.Window("Converter", layout=[[label_feet,input_box_feet],
+                                        [label_inches,input_box_inches],
+                                        [convert_button, label_result]])
+
+
+while True:
+    event, values = window.read()
+    feet = float(values["feet"])
+    inches = float(values["inches"])
+
+    result = convert(feet, inches)
+    window["output"].update(value=f"{result} m", text_color="white")
+
+window.close()
