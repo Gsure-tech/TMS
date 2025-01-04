e49020c0631c0a6e34ed39bd7aaf7f1794713cf6
gsure-tech <Geeescode@>
2025-01-04 15:49:09 +0100

    add exit button

--- bonus/inches_converter.py
+++ bonus/inches_converter.py
@@ -1,16 +1,19 @@
 import FreeSimpleGUI as sg
 from converters14 import convert
 
+
+sg.theme("Black")
 label_feet = sg.Text("Enter feet")
 input_box_feet = sg.InputText(key="feet")
 label_inches = sg.Text("Enter inches")
 input_box_inches = sg.InputText(key="inches")
 convert_button = sg.Button("Convert")
+exit_button = sg.Button("Exit")
 label_result = sg.Text("",key="output")
 
 window = sg.Window("Converter", layout=[[label_feet,input_box_feet],
                                         [label_inches,input_box_inches],
-                                        [convert_button, label_result]])
+                                        [convert_button, exit_button, label_result]])
 
 
 while True:
