299770ffec7c6b850de27148b53354307f167e8d
gsure-tech <Geeescode@>
2025-01-06 14:40:21 +0100

    modify password check

--- bonus/bonus2.1.py
+++ bonus/bonus2.1.py
@@ -3,4 +3,7 @@ password = input("Enter password: ")
 while password != "pass123":
     password = input("Enter password: ")
 
+if len(password) < 8:
+    print("Password is too short")
+
 print("Password is correct!")
